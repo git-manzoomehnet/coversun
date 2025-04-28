1a9142558e427020fdb92552f211718fab6f2323
Zeinab ansarian <zeinabansarian6280@gmail.com>
2025-04-28 17:05:19 +0330

    new change

--- web/assets/js/product.js
+++ web/assets/js/product.js
@@ -17,6 +17,16 @@ const gallerySlider = new Swiper('.gallerySlider', {
 
    })
 
+   const relatedBlog = new Swiper('.relatedBlog', {
+    speed:1000,
+    spaceBetween:116,
+    slidesPerView:"auto",
+   });
+ const relatedProduct = new Swiper('.relatedProduct', {
+    speed:1000,
+    spaceBetween:99.73,
+    slidesPerView:"auto",
+   });
 
 
 
