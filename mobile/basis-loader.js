435d35726f7743bc7a0b1bbece57eaf5f27ade27
Pooya <pooya9685@gmail.com>
2025-05-01 02:17:07 +0330

    add

--- mobile/basis-loader.js
+++ mobile/basis-loader.js
@@ -12,32 +12,15 @@
         "default.binding.regex": "\\{##([^#]*)##\\}",
       },
     };
-  
-    document.addEventListener('DOMContentLoaded', () => {
-      const CMS_CDN_URL = window.host.settings['cms.cms.cdn'];
-      document.querySelectorAll('*').forEach(element => {
-          if (element.innerHTML.includes('[##cms.cms.cdn##]')) {
-              element.innerHTML = element.innerHTML.replace(
-                  /\[##cms.cms.cdn##\]/g,
-                  CMS_CDN_URL
-              );
-          }
-      });
-      document.querySelectorAll('[data-cms-cdn]').forEach(element => {
-          if (element.src) {
-              element.src = element.src.replace('[##cms.cms.cdn##]', CMS_CDN_URL);
-          }
-          if (element.href) {
-              element.href = element.href.replace('[##cms.cms.cdn##]', CMS_CDN_URL);
-          }
-      });
-  });
 
   const basiscoreScript = document.createElement('script');
   basiscoreScript.src = "https://basispanel.ir/_js/basiscore-2.30.1.min.js";
   basiscoreScript.async = true;
   document.head.appendChild(basiscoreScript);
 
+  document.addEventListener("DOMContentLoaded", () => {
+    document.body.innerHTML = document.body.innerHTML.replaceAll("[##cms.cms.cdn##]", "http://J110Xu.UnderTest.ir");
+  });
   const alasqlScript = document.createElement('script');
   alasqlScript.src = "https://cdn.basiscore.net/basispanel.ir/_js/alasql.min.js";
   alasqlScript.async = true;
